665b3a30d5d5e8116ca3632936824e0c8725bac7
wwbweibo <wwbweibo@outlook.com>
2020-12-15 22:46:57 +0800

    完善Buffer的读写功能

--- server/byte_buffer.go
+++ server/byte_buffer.go
@@ -1,5 +1,11 @@
 package server
 
+import "math"
+
+const (
+	maxBufferSize = math.MaxInt32
+)
+
 type ByteBuffer struct {
 	readerIndex int
 	writerIndex int
@@ -16,18 +22,76 @@ func NewByteBuffer() *ByteBuffer {
 }
 
 func (b *ByteBuffer) Read(p []byte) (n int, err error) {
-	if len(b.buffer) < len(p) {
-		length := len(b.buffer)
-		copy(p, b.buffer)
-		b.buffer = []byte{}
-		return length, nil
+	readLength := len(p)
+	readableLength := b.getReadableBytesLength()
+
+	if readableLength >= readLength {
+		copy(p, b.buffer[b.readerIndex:b.readerIndex+readLength])
+		b.readerIndex = b.readerIndex + readLength
+		return readLength, nil
 	} else {
-		copy(p, b.buffer[0:len(p)])
-		return len(p), nil
+		copy(p, b.buffer[b.readerIndex:b.readerIndex+readableLength])
+		b.readerIndex += readableLength
+		return readableLength, nil
 	}
 }
 
 func (b *ByteBuffer) Write(p []byte) (n int, err error) {
-	b.buffer = append(b.buffer, p...)
-	return len(p), nil
+	writeLength := len(p)
+	writableLength := b.getWritableBytesLength()
+	if writeLength > writableLength {
+		// increase the buffer size
+		b.organizeSpace(b.bufferSize - b.readerIndex + 2*writeLength)
+	}
+	writableLength = b.getWritableBytesLength()
+	// enrich the max limit
+	if writeLength > writableLength {
+		copy(b.buffer[b.writerIndex:b.writerIndex+writableLength], p[0:writableLength])
+		b.writerIndex += writableLength
+		return writableLength, nil
+	} else {
+		copy(b.buffer[b.writerIndex:b.writerIndex+writeLength], p[:])
+		b.writerIndex += writeLength
+		return writeLength, nil
+	}
+}
+
+func (b *ByteBuffer) GetReaderIndex() int {
+	return b.readerIndex
+}
+
+func (b *ByteBuffer) GetWriterIndex() int {
+	return b.writerIndex
+}
+
+func (b *ByteBuffer) SetReaderIndex(index int) {
+	b.readerIndex = index
+}
+
+func (b ByteBuffer) SetWriterIndex(index int) {
+	b.writerIndex = index
+}
+
+func (b *ByteBuffer) getReadableBytesLength() int {
+	return b.writerIndex - b.readerIndex
+}
+
+func (b *ByteBuffer) getWritableBytesLength() int {
+	return b.bufferSize - b.writerIndex
+}
+
+func (b ByteBuffer) organizeSpace(expect int) {
+	// delete the used space
+	cleanableBytes := b.readerIndex
+	b.buffer = b.buffer[0:cleanableBytes]
+	b.bufferSize -= cleanableBytes
+	buffersize := 0
+	if expect > maxBufferSize {
+		buffersize = maxBufferSize
+	} else {
+		buffersize = expect
+	}
+	reclaimSpace := buffersize - b.bufferSize
+	b.buffer = append(b.buffer, make([]byte, reclaimSpace)...)
+	b.bufferSize = buffersize
 }
